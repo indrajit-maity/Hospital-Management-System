fd5b395a78c739889649fe4ec4ca6297e80cb0a0
Indrajit Maity <2005indrajitmaity@gmail.com>
2026-07-10 22:17:51 +0530

    fixed web security

--- src/main/java/com/HospitalManagement/ManagedHospital/Security/WebSecurityConfig.java
+++ src/main/java/com/HospitalManagement/ManagedHospital/Security/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.HospitalManagement.ManagedHospital.Security;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @RequiredArgsConstructor
